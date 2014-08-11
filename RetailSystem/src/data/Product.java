c9a54749ed491af91c4e86bcb2d5e6cc8597e387
sdouglas769 <sdouglas769@gmail.com>
2014-08-11 13:44:50 +0100

    finished Product Skeleton

--- RetailSystem/src/data/Product.java
+++ RetailSystem/src/data/Product.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 
 public class Product {
 	
+	private static int nextId;
 	private int id;
 	private String name;
 	private Supplier supplier;
-	private boolean available;
+	private boolean available = false;
 	private double price;
-	private boolean deleted;
+	private boolean deleted = false;
 	private boolean discounted;
 	private double discountedPercentage;
 	private int quantity;
@@ -18,12 +19,20 @@ public class Product {
 	
 	private ArrayList<Product> products = new ArrayList<Product>();
 	
-	public Product(int id, String name, String category,  int quantity, double price, Supplier supplier){
+	public Product(){
+		
+	}
+	
+	public Product(String name, String category,  int quantity, double price, Supplier supplier, boolean available, int lowStockOrder){
 		this.name = name;
 		this.category = category;
 		this.quantity = quantity;
 		this.price = price;
 		this.supplier = supplier;
+		this.available = available;
+		this.lowStockOrder = lowStockOrder;
+		nextId++;
+		this.id = nextId;
 	}
 	
 	public String getName(){
@@ -54,8 +63,82 @@ public class Product {
 		return price;
 	}
 	
-	public void setPrice(){
+	public void setPrice(double price){
 		this.price = price;
+	}
+	
+	public boolean upDateProducts(){
+		return false;
+	}
+	
+	public double applyDiscount(double amount){
+		this.discountedPercentage = amount /100;
+		this.discounted = true;
+		return this.price * discountedPercentage - price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Supplier getSupplier() {
+		return supplier;
+	}
+
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public boolean isDiscounted() {
+		return discounted;
+	}
+
+	public void setDiscounted(boolean discounted) {
+		this.discounted = discounted;
+	}
+
+	public double getDiscountedPercentage() {
+		return discountedPercentage;
+	}
+
+	public void setDiscountedPercentage(double discountedPercentage) {
+		this.discountedPercentage = discountedPercentage;
+	}
+
+	public int getLowStockOrder() {
+		return lowStockOrder;
+	}
+
+	public void setLowStockOrder(int lowStockOrder) {
+		this.lowStockOrder = lowStockOrder;
+	}
+
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(ArrayList<Product> products) {
+		this.products = products;
 	}
 
 }
