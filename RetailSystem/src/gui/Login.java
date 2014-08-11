76c97dce6d97a3e537e6578298806549026bf6ee
Gerry <gerard.nugent82@gmail.com>
2014-08-11 13:27:01 +0100

    Populated Customer ArrayList

--- RetailSystem/src/gui/Login.java
+++ RetailSystem/src/gui/Login.java
@@ -19,7 +19,7 @@ public class Login extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
