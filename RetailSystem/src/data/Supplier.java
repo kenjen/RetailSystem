06c00922cd44ab6c784388a201ea881b8a26caf1
MihaelaPatularu <patularumc@yahoo.com>
2014-08-11 12:44:27 +0100

    created Supplier class

---
+++ RetailSystem/src/data/Supplier.java
@@ -0,0 +1,39 @@
+package data;
+
+public class Supplier {
+	private int id;
+	private String name;
+	private String address;
+	private boolean deleted=false;
+
+	public Supplier(int id, String name, String address) {
+		this.id=id;
+		this.name=name;
+		this.address=address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
